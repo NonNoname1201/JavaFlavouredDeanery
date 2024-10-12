70c8c8f1032fedf45925eb2849d4913b4ae7d1c0
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-10-12 17:44:07 +0200

    update on server files

---
+++ Java flavoured shop storage server/src/main/java/com/jfss/java_flavoured_shop_storage_server/entity/Product.java
@@ -0,0 +1,17 @@
+package com.jfss.java_flavoured_shop_storage_server.entity;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Product {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//    private Double price;
+//
+//    // Getters and Setters
+//}
