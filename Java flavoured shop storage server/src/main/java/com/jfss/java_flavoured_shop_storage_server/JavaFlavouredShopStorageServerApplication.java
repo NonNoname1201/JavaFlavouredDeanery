0c14fce7780c88e2d47803240bb7a6725ca14fd2
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-10-12 17:11:13 +0200

    Initial creation of spring api

---
+++ Java flavoured shop storage server/src/main/java/com/jfss/java_flavoured_shop_storage_server/JavaFlavouredShopStorageServerApplication.java
@@ -0,0 +1,13 @@
+package com.jfss.java_flavoured_shop_storage_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFlavouredShopStorageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFlavouredShopStorageServerApplication.class, args);
+	}
+
+}
---
+++ Java flavoured shop storage server/src/test/java/com/jfss/java_flavoured_shop_storage_server/JavaFlavouredShopStorageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfss.java_flavoured_shop_storage_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFlavouredShopStorageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
